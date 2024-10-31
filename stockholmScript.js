cb976b5b501af0f0dbe4154532507aa832ad531b
MoysisPap <144357526+MoysisPap@users.noreply.github.com>
2024-10-31 13:44:49 +0100

    pin update

--- stockholmScript.js
+++ stockholmScript.js
@@ -83,6 +83,7 @@ let marker;
 
 function initMap() {
   const initialLocation = { lat: 59.33091976142107, lng: 18.060195177256297 };
+  const markerOffset = 0.0025;
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: initialLocation,
@@ -93,7 +94,7 @@ function initMap() {
     map: map,
     draggable: true,
     animation: google.maps.Animation.DROP,
-    position: initialLocation,
+    position: { lat: initialLocation.lat + markerOffset, lng: initialLocation.lng },
   });
 
   google.maps.event.addListener(marker, 'dragend', function () {
