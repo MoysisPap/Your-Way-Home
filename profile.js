f9d5557c72f833f63d747e741d05f5d5c018c81f
MoysisPap <144357526+MoysisPap@users.noreply.github.com>
2024-11-22 11:34:35 +0100

    update

---
+++ profile.js
@@ -0,0 +1,24 @@
+// Google Maps code
+let map;
+let marker;
+
+function initMap() {
+  const initialLocation = { lat: 59.33091976142107, lng: 18.060195177256297 };
+  const markerOffset = 0.0010;
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: initialLocation,
+    zoom: 16,
+  });
+
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: initialLocation.lat + markerOffset, lng: initialLocation.lng },
+  });
+
+  google.maps.event.addListener(marker, 'dragend', function () {
+    updateLocationInput(marker.getPosition());
+  });
+}
